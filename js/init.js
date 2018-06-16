f60b6e7350caf11108ff1fcad6d3d374deed4ce6
SOKOL-ПК\SOKOL <sokol.s.wezom>
2018-06-16 18:34:59 +0200

    update from Vue to noVue

---
+++ js/init.js
@@ -0,0 +1,17 @@
+function check_login() {
+    var log = document.getElementById("login").value;
+    var pass = document.getElementById("password").value;
+
+    if (log == "log123") {
+      document.getElementById("tx").innerHTML = log;
+      document.location.href = 'profil.html';
+      if (pass == "pass123") {
+        document.getElementById("txV").innerHTML = pass;
+        document.location.href = 'profil.html';
+      }
+    }
+    else {
+      document.location.href = 'index.html';
+      document.getElementById("tx").innerHTML = "orevuar!";
+    }
+  }
